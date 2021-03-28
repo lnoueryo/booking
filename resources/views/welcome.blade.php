42ed9ad02dd96cbcb89ea393575c2c9b2be49977
lnoueryo <popo62520908@gmail.com>
2021-03-28 20:25:26 +0900

    タイトルとファビコン追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="Reservierungを使って予約管理しませんか？Reservierungはサロン・美容室の無料予約管理システムです。予約受付の自動化、予約情報の管理と効率化を可能にする機能を使ってみましょう。">
+        <meta name="keyword" content="reservierung lnoueryo inoueryo sns 井上領">
+        <meta property="og:url" content="https://lnoueryo98.sakura.ne.jp/booking" />
+        <meta property="og:title" content="Reservierung" />
+        <meta property="og:image" content="/images/logo/favicon.png" />
         <script src="{{ asset('js/app.js') }}"defer></script>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <title>Laravel</title>
-
+        <title>Reservierung</title>
+        {{--  <link rel="shortcut icon" href="/images/logo/logo01.png" />  --}}
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
